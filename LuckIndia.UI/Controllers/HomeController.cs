8c0f70711a804ab09eb622715ac98f53a8ed3fd7
abhagwat <atulabhagwat@gmail.com>
2018-01-05 12:49:53 +0530

    Session

--- LuckIndia.UI/Controllers/HomeController.cs
+++ LuckIndia.UI/Controllers/HomeController.cs
@@ -1,4 +1,5 @@
 ﻿using LuckIndia.Services.RegistrationServices;
+using LuckIndia.UI.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -19,7 +20,7 @@ namespace LuckIndia.UI.Controllers
         public ActionResult About()
         {
             ViewBag.Message = "Welcome to Luck India";
-
+                
             return View();
         }
 
@@ -30,12 +31,56 @@ namespace LuckIndia.UI.Controllers
             return View();
         }
 
+
         public ActionResult Login()
         {
-            
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model)
+        {
+
+            if (Session["UserID"] == null)
+            {
+                if (ModelState.IsValid)
+                {
+                    Registration reg = new Registration();
+
+                    string strError = "";
+                    bool bSuccess = reg.SignIn(model.UserName, model.Password, out strError);
+                    if (bSuccess)
+                    {
+                        Session["UserID"] = model.UserName;
+                        return RedirectToAction("Index", "Quiz");
+
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("", "Incorrect username or password");
+                    }
+                    //ViewBag.Success = bSuccess ? "Successful login" : strError;
+                    //if (bSuccess)
+                    //    return RedirectToAction("About");
+                    //else
+
+                }
+            }
+
+            ModelState.AddModelError("", "User already logged in");
+
+            return RedirectToAction("Login");
+
+        }
+
+
+        public ActionResult SignIn(LoginModel model)
+        {
+           
+            return View(model);
+        }
+
+      
     }
 }
